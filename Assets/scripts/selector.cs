e253e325ad95a1cbedf98943b72ddd5f09c14941
Alan Yoshida <alanyoshida@msn.com>
2020-03-20 00:17:38 -0300

    first commit

---
+++ Assets/scripts/selector.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selector : MonoBehaviour
+{
+  public Camera Cam;
+  private RaycastHit R;
+  private GameObject[] selectedUnits;
+  private Vector2 mouseStartPosition;
+  private Vector2 mouseEndPosition;
+  private Vector2 mouseInitialPosition;
+  public Image selectionBackground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      selectedUnits = GameObject.FindGameObjectsWithTag("ship");
+    }
+
+    void drawSelectionBox()
+    {
+      // when mouse button is down initialize the positions
+      if (Input.GetMouseButtonDown(0))
+      {
+        mouseStartPosition = Input.mousePosition;
+        mouseInitialPosition = Input.mousePosition;
+      }
+
+      if (Input.GetMouseButton(0))
+      {
+        // Control the if you draw from right,left or bottom, up
+        if (Input.mousePosition.x >= mouseInitialPosition.x)
+        {
+          mouseEndPosition.x = Input.mousePosition.x;
+        } else {
+          mouseStartPosition.x = Input.mousePosition.x;
+          mouseEndPosition.x = mouseInitialPosition.x;
+        }
+
+        if (Input.mousePosition.y >= mouseInitialPosition.y)
+        {
+          mouseEndPosition.y = Input.mousePosition.y;
+        } else {
+          mouseStartPosition.y = Input.mousePosition.y;
+          mouseEndPosition.y = mouseInitialPosition.y;
+        }
+
+
+      // Set the image to the desired widths and heights
+        selectionBackground.rectTransform.anchorMax =
+          new Vector2(mouseEndPosition.x / Screen.width, mouseEndPosition.y / Screen.height);
+        selectionBackground.rectTransform.anchorMin =
+          new Vector2(mouseStartPosition.x / Screen.width, mouseStartPosition.y / Screen.height);
+      }
+
+      // when release the mouse it clear the image
+      if (Input.GetMouseButtonUp(0))
+      {
+        selectionBackground.rectTransform.anchorMax = new Vector2(0, 0);
+        selectionBackground.rectTransform.anchorMin = new Vector2(0, 0);
+      }
+    }
+
+    void selectUnits()
+    {
+      if (Input.GetMouseButtonUp(0))
+      {
+        RaycastHit2D PosMinMouse2d = Physics2D.Raycast(Cam.ScreenToWorldPoint(mouseStartPosition), Vector2.zero);
+        RaycastHit2D PosMaxMouse2d = Physics2D.Raycast(Cam.ScreenToWorldPoint(mouseEndPosition), Vector2.zero);
+
+        List<GameObject> items = new List<GameObject>(GameObject.FindGameObjectsWithTag("ship"));
+        int totalUnits = items.
+        foreach (var item in items)
+        {
+          if (collider.IsTouching(item.GetComponent<BoxCollider2D>()))
+          {
+            print("Is Colliding");
+          }
+        }
+
+
+      }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      this.drawSelectionBox();
+      this.selectUnits();
+    }
+}
